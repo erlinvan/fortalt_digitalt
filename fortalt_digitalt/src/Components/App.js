54219fa9cf9d317154d6f5ab099373b27cb4e2b6
MrOctopus <shock.game@hotmail.com>
2021-03-03 11:33:26 +0100

    Added TopBar class, and made material-ui icons a dependency

--- fortalt_digitalt/src/Components/App.js
+++ fortalt_digitalt/src/Components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import { ButtonGroup, Grid } from "@material-ui/core";
+import AppBar from '@material-ui/core/AppBar';
+import Title from "./TopBar";
 import Toggle from "./Toggle";
 
 function App() {
   return (
     <>
+      <Title />
       <Grid container spacing={2} direction="column" alignItems="center">
         <Grid item>
           <h1>Hei!</h1>
