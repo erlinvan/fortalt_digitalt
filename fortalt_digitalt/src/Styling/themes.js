340d758b2958246d6f08532d5998cf150b2cd47e
Anette Sandvig <anetsan@stud.ntnu.no>
2021-03-24 08:47:45 +0100

    Skrev inn litt fler spm

--- fortalt_digitalt/src/Styling/themes.js
+++ fortalt_digitalt/src/Styling/themes.js
@@ -5,11 +5,12 @@ import amber from '@material-ui/core/colors/amber';
 // Icons
 import sun from './sun.png';
 import moon from './moon.png';
+import starry_night from './starry_night.jpg';
 
 export const lightTheme = {
     palette: {
         primary: {
-          main: lightGreen[500],
+          main: "#212F3C",
         },
         secondary: {
           main: amber[500],
@@ -19,10 +20,17 @@ export const lightTheme = {
       MuiCssBaseline: {
         '@global': {
           body: {
+            //backgroundPosition: '0% 10%',
+            //backgroundRepeat: 'no-repeat',
+            //backgroundAttachment: 'fixed',
+            //backgroundSize: "1300px 700px",
+            //backgroundImage: `url(${starry_night})`,
             backgroundPosition: '97% 95%',
             backgroundRepeat: 'no-repeat',
             backgroundAttachment: 'fixed',
-            backgroundImage: `url(${sun})`
+            backgroundImage: `url(${sun})`,
+            backgroundColor: "#34495E"
+
           }
         }
       }
@@ -33,7 +41,7 @@ export const darkTheme = {
     palette: {
         type: 'dark',
         primary: {
-          main: green[500],
+          main: "#2471A3",
         },
         secondary: {
           main: amber[500],
