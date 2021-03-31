931ecb39aa14f80d2b73a8a63e3caa19b3a182af
Anette Sandvig <anetsan@stud.ntnu.no>
2021-03-31 22:20:03 +0200

    Årstid+tidspunkt funksjoner

--- fortalt_digitalt/src/Common/time.js
+++ fortalt_digitalt/src/Common/time.js
@@ -8,3 +8,35 @@ export default function isDay() {
       return false; 
     }
 }
+
+export function getTimeType() {
+  let time = new Date().getHours();
+  if (time > 7 && time < 12) {
+    return "Morgen";
+  }
+  if (time >= 12 && time <= 18) {
+    return "Ettermiddag";
+  }
+  if (time > 18 && time <= 24) {
+    return "Kveld";
+  }
+  if (time > 24 && time <= 7) {
+    return "Natt";
+  }
+}
+
+export function getSeason() {
+    let month = new Date().getMonth();
+      if (month < 2 || month >= 11) {
+    return "Vinter";
+  }
+    if (month >= 2 && month <=4 ) {
+      return "Vår";
+  }
+    if (month >= 5 && month <= 7 ) {
+      return "Sommer";
+  }
+    if (month >=8 && month <= 10) {
+      return "Høst";
+  }
+}
